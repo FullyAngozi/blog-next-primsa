d8d485c0a44d72f2f2225bd47bba2d2b7a46b0a1
FullyAngozi <ochuks33@yahoo.com>
2024-01-19 12:31:01 -0500

    changed sessions to session in the SessionProvider wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import { AppProps } from "next/app";
 import { SessionProvider } from 'next-auth/react';
+import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <SessionProvider session={pageProps.sessions}>
+    <SessionProvider session={pageProps.session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
